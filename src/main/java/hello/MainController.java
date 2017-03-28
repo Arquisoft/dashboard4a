583e08ad94187a0c553e26cdba10f721988f58f7
DANIELTOIMIL\Daniel <djlt93@gmail.com>
2017-03-28 21:02:07 +0200

    HTML5 sent event añadido. Prueba de un producer.

--- src/main/java/hello/MainController.java
+++ src/main/java/hello/MainController.java
@@ -2,9 +2,11 @@ package hello;
 
 
 import org.apache.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,6 +19,7 @@ public class MainController {
 
     private static final Logger logger = Logger.getLogger(MainController.class);
     private List<SseEmitter> sseEmitters = Collections.synchronizedList(new ArrayList<>());
+    
 
     @RequestMapping("/")
     public String landing(Model model) {
@@ -24,5 +27,33 @@ public class MainController {
     }
 
 
+    @RequestMapping("/updates")
+	SseEmitter subscribeUpdates() {
+		SseEmitter sseEmitter = new SseEmitter();
+		synchronized (this.sseEmitters) {
+			this.sseEmitters.add(sseEmitter);
+			sseEmitter.onCompletion(() -> {
+				synchronized (this.sseEmitters) {
+					this.sseEmitters.remove(sseEmitter);
+				}
+			});
+		}
+		return sseEmitter;
+	}
+    
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+	@KafkaListener(topics="exampleTopic")
+	public String showMessage(String data) {
+		
+			for (SseEmitter sseEmitter : this.sseEmitters) {
+				try {
+					sseEmitter.send(data);
+				} catch (Exception e) {
+					logger.error("Se ha cerrado el navegador");
+				}
+			}
+		
+		return data;
+	}
 
 }
